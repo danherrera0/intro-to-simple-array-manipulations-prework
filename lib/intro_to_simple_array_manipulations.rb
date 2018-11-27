2301b1288e5b39020c40496fe337b547ef7e9b03
Dania Herrera <danherrera0@gmail.com>
2018-11-27 15:22:11 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,12 @@
-artists = ["Hozier","Ariana Grande","Usher"]
-next_artist = "Beyonce"
-artists.push("next_artist")
 
 def using_push(countries_in_western_africa, next_country)
 countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
 next_country = "Niger"
 return countries_in_western_africa.push(next_country)
-end 
+end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn)
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  new_neighborhood = "Brooklyn Heights"
+  return neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
+end
