87caa311accad2b0f65abd7bf0e577fb01bb7f5b
Dania Herrera <danherrera0@gmail.com>
2018-11-27 15:20:41 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,9 @@
 artists = ["Hozier","Ariana Grande","Usher"]
 next_artist = "Beyonce"
 artists.push("next_artist")
+
+def using_push(countries_in_western_africa, next_country)
+countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+next_country = "Niger"
+return countries_in_western_africa.push(next_country)
+end 
